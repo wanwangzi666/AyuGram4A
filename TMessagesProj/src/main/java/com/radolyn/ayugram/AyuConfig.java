0ace702733cfe1a4a0a9c374765af684ced066e2
ZavaruKitsu <alexeyzavar@gmail.com>
2023-05-29 23:48:15 +0300

    feat: kang Extera's config

--- TMessagesProj/src/main/java/com/radolyn/ayugram/AyuConfig.java
+++ TMessagesProj/src/main/java/com/radolyn/ayugram/AyuConfig.java
@@ -1,5 +1,38 @@
 package com.radolyn.ayugram;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+import org.telegram.messenger.ApplicationLoader;
+
 public class AyuConfig {
+    private static final Object sync = new Object();
+    private static boolean configLoaded;
+
+    public static SharedPreferences preferences;
+    public static SharedPreferences.Editor editor;
+
+
     public static boolean enableAds;
+
+
+    static {
+        loadConfig();
+    }
+
+    public static void loadConfig() {
+        synchronized (sync) {
+            if (configLoaded) {
+                return;
+            }
+
+            preferences = ApplicationLoader.applicationContext.getSharedPreferences("ayuconfig", Activity.MODE_PRIVATE);
+            editor = preferences.edit();
+
+            // ...
+            enableAds = preferences.getBoolean("enableAds", true);
+
+            configLoaded = true;
+        }
+    }
 }
